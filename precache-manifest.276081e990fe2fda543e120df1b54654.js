c48b2e85181e5170c195f7e8776ff6860519f5bd
letelete <brunokawka@gmail.com>
2020-05-21 22:20:36 +0200

    Updates

---
+++ precache-manifest.276081e990fe2fda543e120df1b54654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3461005fe7fd849a51bd5a8fb158b2f",
+    "url": "/ch0-3d-animated-button/index.html"
+  },
+  {
+    "revision": "1e85b160363535db937e",
+    "url": "/ch0-3d-animated-button/static/css/main.4f2ac63e.chunk.css"
+  },
+  {
+    "revision": "8f690f4801cce0042dfd",
+    "url": "/ch0-3d-animated-button/static/js/2.c767e119.chunk.js"
+  },
+  {
+    "revision": "6ead315f60788879c491a87054985612",
+    "url": "/ch0-3d-animated-button/static/js/2.c767e119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e85b160363535db937e",
+    "url": "/ch0-3d-animated-button/static/js/main.fb7e166e.chunk.js"
+  },
+  {
+    "revision": "6af3fa9d9d1651cdcde1",
+    "url": "/ch0-3d-animated-button/static/js/runtime-main.700a7c84.js"
+  }
+]);
